026eb942dd2a6c701583d61a4736fa37dd1cb830
Bryan Klingler <bklingler89@gmail.com>
2019-09-24 18:47:03 -0400

    Initial Commit

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Bryan Klingler on 24/09/2019.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+  
+
+}
+
